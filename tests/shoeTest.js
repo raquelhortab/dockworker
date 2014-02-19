d30ae5545bc94f1b5616cf0559509a73f00b3382
AnandkumarPatel <anandkumarpatel@gmail.com>
2014-02-19 12:06:18 -0800

    remove useless stuff

--- tests/shoeTest.js
+++ tests/shoeTest.js
@@ -34,36 +34,4 @@ describe('Streams', function () {
       });
     });
   });
-  describe('logs', function () {
-    before(function () {
-      this.stream = new ShoeClient('ws://localhost:15000/streams/log');
-    });
-    it('should say hi', function (done) {
-      var muxDemux = new MuxDemux(onStream);
-      this.stream.pipe(muxDemux).pipe(this.stream);
-      function onStream(stream) {
-        if (stream.meta === 'terminal') {
-          onTerminal(stream);
-        }
-      }
-      function onTerminal(stream) {
-        stream.on('data', function (data) {
-          if (data === 'HELLO\r\n') {
-            done();
-          }
-        });
-      }
-    });
-    it('should have web up', function (done) {
-      request('http://localhost:15000/api/checkWebUp', function (err, res, body) {
-        if (err) {
-          done(err);
-        } else if (res.statusCode !== 200) {
-          done(new Error());
-        } else {
-          done();
-        }
-      });
-    });
-  });
 });
